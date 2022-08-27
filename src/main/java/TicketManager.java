97a6359cabaa29287f2f97ac617b56f99eff89c6
KlimovaTE <t_klimova@inbox.ru>
2022-08-27 10:12:57 +0300

    Lesson15 Task2

--- src/main/java/TicketManager.java
+++ src/main/java/TicketManager.java
@@ -1,27 +1,41 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class TicketManager {
-protected TicketRepository repo;
+    protected TicketRepository repo;
 
     public TicketManager(TicketRepository repo) {
         this.repo = repo;
     }
 
-    public void addNew(Ticket newTicket)
-    {
+    public void addNew(Ticket newTicket) {
         repo.save(newTicket);
     }
 
-public Ticket[] findAllManager(String from, String to) {
-       Ticket[] result = new Ticket[0];
-    TicketRepository tmp = new TicketRepository();
-    for (Ticket ticket : repo.findAll()) {
-        if (ticket.matches(from, to)) {
-            tmp.save(ticket);
-            result = tmp.findAll();
+    public Ticket[] findAllSortByPrice(String from, String to) {
+        Ticket[] result = new Ticket[0];
+        TicketRepository tmp = new TicketRepository();
+        for (Ticket ticket : repo.findAll()) {
+            if (ticket.matches(from, to)) {
+                tmp.save(ticket);
+                result = tmp.findAll();
+            }
         }
+        Arrays.sort(result);
+        return result;
     }
-    Arrays.sort(result);
-    return result;
-}
+
+    public Ticket[] findAllSortByTravelTime(String from, String to, Comparator<Ticket> comparator) {
+        Ticket[] result = new Ticket[0];
+        TicketRepository tmp = new TicketRepository();
+        for (Ticket ticket : repo.findAll()) {
+            if (ticket.matches(from, to)) {
+                tmp.save(ticket);
+                result = tmp.findAll();
+            }
+        }
+        Arrays.sort(result, comparator);
+        return result;
+    }
+
 }
